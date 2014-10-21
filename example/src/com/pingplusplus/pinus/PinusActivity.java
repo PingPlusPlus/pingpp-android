2df70c00b4131ca5b3e7023c5c59a6a4049a3b67
Xufeng Weng <wengxufeng@gmail.com>
2014-10-21 18:19:21 +0800

    format code

--- example/src/com/pingplusplus/pinus/PinusActivity.java
+++ example/src/com/pingplusplus/pinus/PinusActivity.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.text.NumberFormat;
 
-
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
@@ -18,7 +17,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-
 import com.google.gson.Gson;
 import com.pingplusplus.android.PaymentActivity;
 import com.squareup.okhttp.MediaType;
@@ -26,8 +24,6 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
-
-
 
 public class PinusActivity extends Activity implements View.OnClickListener{
 
@@ -54,104 +50,99 @@ public class PinusActivity extends Activity implements View.OnClickListener{
         wechatButton = (Button) findViewById(R.id.wechatButton);
         alipayButton = (Button) findViewById(R.id.alipayButton);
         upmpButton = (Button) findViewById(R.id.upmpButton);
-         wechatButton.setOnClickListener(PinusActivity.this);
+        wechatButton.setOnClickListener(PinusActivity.this);
         alipayButton.setOnClickListener(PinusActivity.this);
         upmpButton.setOnClickListener(PinusActivity.this);
     
-    amountEditText.addTextChangedListener(new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            if (!s.toString().equals(currentAmount)) {
-                amountEditText.removeTextChangedListener(this);
-
-                String replaceable = String.format("[%s,.]", NumberFormat.getCurrencyInstance().getCurrency().getSymbol());
-                String cleanString = s.toString().replaceAll(replaceable, "");
-
-                if (new BigDecimal(cleanString).toString().equals("0")) {
-                    amountEditText.setText(null);
-                } else {
-                    double parsed = Double.parseDouble(cleanString);
-                    String formatted = NumberFormat.getCurrencyInstance().format((parsed / 100));
-                    currentAmount = formatted;
-                    amountEditText.setText(formatted);
-                    amountEditText.setSelection(formatted.length());
-                }
-                amountEditText.addTextChangedListener(this);
+        amountEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
-        }
-    });
-}
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
 
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!s.toString().equals(currentAmount)) {
+                    amountEditText.removeTextChangedListener(this);
 
-   public void onClick(View view) {
-       String amountText = amountEditText.getText().toString();
-       if (amountText.equals("")) return;
+                    String replaceable = String.format("[%s,.]", NumberFormat.getCurrencyInstance().getCurrency().getSymbol());
+                    String cleanString = s.toString().replaceAll(replaceable, "");
 
-       String replaceable = String.format("[%s,.]", NumberFormat.getCurrencyInstance().getCurrency().getSymbol());
-       String cleanString = amountText.toString().replaceAll(replaceable, "");
-       int amount = Integer.valueOf(new BigDecimal(cleanString).toString());
-
-       // 支付宝，微信支付，银联按键的点击响应处理
-       if (view.getId() == R.id.upmpButton) {
-           new PaymentTask().execute(new PaymentRequest(CHANNEL_UPMP, amount));
-       } else if (view.getId() == R.id.alipayButton) {
-           new PaymentTask().execute(new PaymentRequest(CHANNEL_ALIPAY, amount));
-       } else if (view.getId() == R.id.wechatButton) {
-           new PaymentTask().execute(new PaymentRequest(CHANNEL_WECHAT, amount));
-       }
-   }
-
-class PaymentTask extends AsyncTask<PaymentRequest, Void, String> {
-
-    @Override
-    protected void onPreExecute() {
-
-        //按键点击之后的禁用，防止重复点击
-        wechatButton.setOnClickListener(null);
-        alipayButton.setOnClickListener(null);
-        upmpButton.setOnClickListener(null);
-}
-
-    @Override
-    protected String doInBackground(PaymentRequest... pr) {
-
-        PaymentRequest paymentRequest = pr[0];
-        String data = null;
-        String json = new Gson().toJson(paymentRequest);
-        try {
-
-            //向Your Ping++ Server SDK请求数据
-            data = postJson(URL, json);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return data;
+                    if (new BigDecimal(cleanString).toString().equals("0")) {
+                        amountEditText.setText(null);
+                    } else {
+                        double parsed = Double.parseDouble(cleanString);
+                        String formatted = NumberFormat.getCurrencyInstance().format((parsed / 100));
+                        currentAmount = formatted;
+                        amountEditText.setText(formatted);
+                        amountEditText.setSelection(formatted.length());
+                    }
+                    amountEditText.addTextChangedListener(this);
+                }
+            }
+        });
     }
 
-    @Override
-    protected void onPostExecute(String data) {
+    public void onClick(View view) {
+        String amountText = amountEditText.getText().toString();
+        if (amountText.equals("")) return;
 
-        Intent intent = new Intent();
-        String packageName = getPackageName();
-        ComponentName componentName = new ComponentName(packageName, packageName + ".wxapi.WXPayEntryActivity");
-        intent.setComponent(componentName);
-        intent.putExtra(PaymentActivity.EXTRA_CHARGE,data);
-        startActivityForResult(intent, REQUEST_CODE_PAYMENT);
+        String replaceable = String.format("[%s,.]", NumberFormat.getCurrencyInstance().getCurrency().getSymbol());
+        String cleanString = amountText.toString().replaceAll(replaceable, "");
+        int amount = Integer.valueOf(new BigDecimal(cleanString).toString());
 
+        // 支付宝，微信支付，银联按键的点击响应处理
+        if (view.getId() == R.id.upmpButton) {
+            new PaymentTask().execute(new PaymentRequest(CHANNEL_UPMP, amount));
+        } else if (view.getId() == R.id.alipayButton) {
+            new PaymentTask().execute(new PaymentRequest(CHANNEL_ALIPAY, amount));
+        } else if (view.getId() == R.id.wechatButton) {
+            new PaymentTask().execute(new PaymentRequest(CHANNEL_WECHAT, amount));
+        }
     }
 
+    class PaymentTask extends AsyncTask<PaymentRequest, Void, String> {
 
-}
+            @Override
+            protected void onPreExecute() {
+
+                //按键点击之后的禁用，防止重复点击
+                wechatButton.setOnClickListener(null);
+                alipayButton.setOnClickListener(null);
+                upmpButton.setOnClickListener(null);
+        }
+
+        @Override
+        protected String doInBackground(PaymentRequest... pr) {
+
+            PaymentRequest paymentRequest = pr[0];
+            String data = null;
+            String json = new Gson().toJson(paymentRequest);
+            try {
+                //向Your Ping++ Server SDK请求数据
+                data = postJson(URL, json);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String data) {
+
+            Intent intent = new Intent();
+            String packageName = getPackageName();
+            ComponentName componentName = new ComponentName(packageName, packageName + ".wxapi.WXPayEntryActivity");
+            intent.setComponent(componentName);
+            intent.putExtra(PaymentActivity.EXTRA_CHARGE,data);
+            startActivityForResult(intent, REQUEST_CODE_PAYMENT);
+
+        }
+
+    }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
@@ -180,7 +171,6 @@ class PaymentTask extends AsyncTask<PaymentRequest, Void, String> {
         }
     }
 
-
     private static String postJson(String url, String json) throws IOException {
         MediaType type = MediaType.parse("application/json; charset=utf-8");
         RequestBody body = RequestBody.create(type, json);
@@ -191,8 +181,6 @@ class PaymentTask extends AsyncTask<PaymentRequest, Void, String> {
 
         return response.body().string();
     }
-
-
 
     class PaymentRequest {
         String channel;
