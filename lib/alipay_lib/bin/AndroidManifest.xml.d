704fc14ae7710390ab8748dd6a3ef18a8f28c23a
Xufeng Weng <wengxufeng@gmail.com>
2014-10-23 10:15:38 +0800

    remove generated files

--- lib/alipay_lib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/dayou/pingpp-sdk/pingpp-android/alipay_lib/bin/AndroidManifest.xml : \
-/home/dayou/pingpp-sdk/pingpp-android/alipay_lib/AndroidManifest.xml \
--- lib/alipay_lib/gen/com/alipay/android/app/sdk/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.alipay.android.app.sdk;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = false;
-}
