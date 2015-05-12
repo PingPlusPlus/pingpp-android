ecd301cd784838a820e125c7ed140ea761c74316
sunkai <burulangtu@gmail.com>
2015-05-12 17:15:28 +0800

    add androidStudio demo

--- README.md
+++ README.md
@@ -3,19 +3,22 @@
 
 ## 简介
 
-[lib_androidstudio](/lib_androidstudio) 目录是 Android Studio 开发环境需要的 aar 文件。<br>
-[example_androidstudio](/example_androidstudio) 目录是一个简单的接入示例，该示例仅供参考。<br>
+[lib_androidstudio](/lib_androidstudio) 目录是 AndroidStudio 开发环境需要的 aar 文件。<br>
+[example_androidstudio](/example_androidstudio) 目录是两个AndroidStudio开发环境的接入示例。分别是使用aar、jar方式，示例仅供参考。<br>
 
 [lib_eclipse](/lib_eclipse) 目录下是 Eclipse 开发环境需要的第三方库。<br>
 [libs](/lib_eclipse/libs) 目录下的是 jar 文件。<br>
 [bdwallet\_pay\_sdk](/lib_eclipse/bdwallet_pay_sdk) 是 Library Project。<br>
-[example_eclipse](/example_eclipse) 文件夹里面是一个简单的接入示例，该示例仅供参考。<br>
+[example_eclipse](/example_eclipse) 文件夹里面是一个Eclipse开发环境的接入示例，该示例仅供参考。<br>
 
 安卓端如果应用没有安装银联支付控件：请到 [这里](http://mobile.unionpay.com/getclient?platform=android&type=securepayplugin) 下载。
 
 ## 版本要求
 Android SDK 要求 Android 2.3 及以上版本
 
+##注意事项
+
+壹收款可能会与友盟、百度地图等其他第三方jar包冲突，当同时使用这些jar包的时候用户需要根据情况判断保留哪一方的jar包。
 
 ##版本1.0.1
 1、申请Charge的时候，增加一个扩展字段extras。extras的各位为一个json对象，接收参数的方式由用户自行解析。<br>
---
+++ example_androidstudio/OneDemo-aar/app/src/main/java/com/jianmi/jmall/MainActivity.java
@@ -0,0 +1,56 @@
+package com.jianmi.jmall;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.pingplusplus.libone.PayActivity;
+
+/**
+ * Created by sunkai on 15/3/17.
+ */
+public class MainActivity extends Activity implements View.OnClickListener {
+    public static final String URL = "YOUR_URL";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+        //设置要使用的支付方式
+        PayActivity.SHOW_CHANNEL_WECHAT = true;
+        PayActivity.SHOW_CHANNEL_UPMP = true;
+        PayActivity.SHOW_CHANNEL_BFB = true;
+        PayActivity.SHOW_CHANNEL_ALIPAY = true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                startActivity(new Intent(MainActivity.this, ExampleActivity.class));
+                break;
+            case R.id.button2:
+                String bill = " {\"order_no\":\"201503230255221\",\"amount\":100,\"display\":[{\"name\":\"商品\",\"contents\":[\"橡胶花盆 x 1\",\"搪瓷水壶 x 1\",\"扫把和簸箕 x 1\"]}]}";
+                PayActivity.CallPayActivity(this, bill, URL);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PayActivity.PAYACTIVITY_REQUEST_CODE) {
+            if (resultCode == PayActivity.PAYACTIVITY_RESULT_CODE) {
+                Toast.makeText(this, data.getExtras().getString("result"), Toast.LENGTH_LONG).show();
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                //pressed back
+                Toast.makeText(this, data.getExtras().getString("result"), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
